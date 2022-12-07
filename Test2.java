c1246cb7aa1f8415891732bfff8037596f31a435
Sam Waggoner <79596351+SamWaggoner@users.noreply.github.com>
2022-12-07 00:50:04 -0500

    Delete Test2.java

--- Test2.java
+++
@@ -1,6 +0,0 @@
-public class Test2 extends Tester{
-    public static String printme2(){
-        System.out.println("Hello from the second Test2!");
-        return "Returned from the second Test2!";
-    }
-}
