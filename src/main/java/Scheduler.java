02dc83945ec61341d1dd90ffe8d07b4d94d5d0c0
Sam Waggoner <samwags3@gmail.com>
2022-12-07 12:14:37 -0500

    removed commented out google calendar connection

--- src/main/java/Scheduler.java
+++ src/main/java/Scheduler.java
@@ -222,12 +222,9 @@ public class Scheduler {
     //     weeks.add(week);
     //   }
     // }
-
-
-
-
   }
 
+  
   public static void checkDue(ArrayList<Task> arr)
   {
     String res = "not failed due";
@@ -240,144 +237,11 @@ public class Scheduler {
     }
     System.out.println(res);
   }
-
-  
 }
 
 
 
 
 
-// TaskList.n = 5;
-// int testN1 = TaskList.incN();
-// int testN2 = TaskList.incN();
-// System.out.println(testN1);
-// System.out.println(testN2);
 
 
-
-// TaskList.printTasks();
-// TaskList.sortBy("hey");
-// System.out.print("HEYYYYLOO");
-// TaskList.printTasks();
-
-// LocalDateTime now = LocalDate.now().atTime(5, 30, 00);
-// Task x = new Task("myLabel", 1, 2.0, now);
-// tasks.set(0,x);
-
-
-
-
-
-
-// From Google Calendar:
-
-// import com.google.api.client.auth.oauth2.Credential;
-// import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-// import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-// import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-// import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-// import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-// import com.google.api.client.http.javanet.NetHttpTransport;
-// import com.google.api.client.json.JsonFactory;
-// import com.google.api.client.json.gson.GsonFactory;
-// import com.google.api.client.util.DateTime;
-// import com.google.api.client.util.store.FileDataStoreFactory;
-// import com.google.api.services.calendar.Calendar;
-// import com.google.api.services.calendar.CalendarScopes;
-// import com.google.api.services.calendar.model.Event;
-// import com.google.api.services.calendar.model.Events;
-
-// import java.io.FileNotFoundException;
-// import java.io.InputStream;
-// import java.io.InputStreamReader;
-// import java.security.GeneralSecurityException;
-// import java.util.Collections;
-// import java.util.List;
-
-
-  // /**
-  //  * Application name.
-  //  */
-  // private static final String APPLICATION_NAME = "Sam's Scheduler";
-  // /**
-  //  * Global instance of the JSON factory.
-  //  */
-  // private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  // /**
-  //  * Directory to store authorization tokens for this application.
-  //  */
-  // private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-  // /**
-  //  * Global instance of the scopes required by this quickstart.
-  //  * If modifying these scopes, delete your previously saved tokens/ folder.
-  //  */
-  // private static final List<String> SCOPES =
-  //     Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
-  // private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-
-  // /**
-  //  * Creates an authorized Credential object.
-  //  *
-  //  * @param HTTP_TRANSPORT The network HTTP Transport.
-  //  * @return An authorized Credential object.
-  //  * @throws IOException If the credentials.json file cannot be found.
-  //  */
-  // private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
-  //     throws IOException {
-  //   // Load client secrets.
-  //   InputStream in = Scheduler.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-  //   if (in == null) {
-  //     throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-  //   }
-  //   GoogleClientSecrets clientSecrets =
-  //       GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-  //   // Build flow and trigger user authorization request.
-  //   GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-  //       HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-  //       .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-  //       .setAccessType("offline")
-  //       .build();
-  //   LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-  //   Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-  //   //returns an authorized Credential object.
-  //   return credential;
-  // }
-
-
-
-
-  // public static void main(String... args) throws IOException, GeneralSecurityException
-  // {
-
-
-  // // Build a new authorized API client service.
-  // final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-  // Calendar service =
-  //     new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-  //         .setApplicationName(APPLICATION_NAME)
-  //         .build();
-
-  // // List the next 10 events from the primary calendar.
-  // DateTime now = new DateTime(System.currentTimeMillis());
-  // Events events = service.events().list("primary")
-  //     .setMaxResults(10)
-  //     .setTimeMin(now)
-  //     .setOrderBy("startTime")
-  //     .setSingleEvents(true)
-  //     .execute();
-  // List<Event> items = events.getItems();
-  // if (items.isEmpty()) {
-  //   System.out.println("No upcoming events found.");
-  // } else {
-  //   System.out.println("Upcoming events");
-  //   for (Event event : items) {
-  //     DateTime start = event.getStart().getDateTime();
-  //     if (start == null) {
-  //       start = event.getStart().getDate();
-  //     }
-  //     System.out.printf("%s (%s)\n", event.getSummary(), start);
-  //   }
-  // }
