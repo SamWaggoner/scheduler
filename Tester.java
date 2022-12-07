1bd0fa8113fcb3089616ff8cde990c4c93d97114
Sam Waggoner <79596351+SamWaggoner@users.noreply.github.com>
2022-12-07 00:50:16 -0500

    Delete Tester.java

--- Tester.java
+++
@@ -1,7 +0,0 @@
-public class Tester {
-    public static String printme() {
-        System.out.println("Hello from test!");
-        String result = "Hello This is the return of Test";
-        return result;
-    }
-}
